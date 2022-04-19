4d64e3b03cbed81b1224b358f50bd3d38206c674
Vasilev Viacheslav <vviace@yandex.ru>
2022-04-19 17:12:32 +0300

    change rep

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void startDialog(){
+    public static void startDialog() {
         System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        System.out.println("Hello, "+name+"!");
+        System.out.println("Hello, " + name + "!");
         sc.close();
     }
 }
